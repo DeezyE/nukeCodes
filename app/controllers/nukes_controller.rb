164c4e3733bcfbed1f17b25cd64a3e3d24b29c62
Isaac Powell <garethclyperis@gmail.com>
2018-04-25 12:58:41 +1000

    refactored nuke code randomiser

--- app/controllers/nukes_controller.rb
+++ app/controllers/nukes_controller.rb
@@ -1,10 +1,11 @@
 class NukesController < ApplicationController
 
   def index
-    seed = (Time.now.strftime("%s").to_i)/3600
-    seed = (Time.now.strftime("%s").to_i)/3600 if seed != (Time.now.strftime("%s").to_i)/3600
-    srand(seed)
-    random = 2.times.map{Random.rand(100000..999999)}
+    seed = (Time.now.strftime("%s").to_i)/3600 #this is run every time index is refreshed but will change only every hour. This represents number of seconds since 1970 divided by 3600 to give hourly value`
+    srand(seed) #sets that hourly second value as the seed for ruby's internal random number generator
+    random = [rand(100000..999999),rand(100000..999999)] # sets random to an array with two 6 digit numbers
+    # the following line will also achieve the same result
+    # random = 2.times.map{rand(100000..999999)}
 
     if current_user.has_role?(:russian)
       @code = "First Code: #{random[0]}, Second Code: #{random[1]}"
